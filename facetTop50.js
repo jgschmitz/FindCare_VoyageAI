fb9161d0e5dfbacbaf68ff4af46de345039ede70
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2025-09-04 15:56:22 -0500

    Delete facetTop50.js

--- facetTop50.js
+++
@@ -1,6 +0,0 @@
-db.taxonomy251.aggregate([
-  { $vectorSearch: { index:"nucc", path:"embedding", queryVector:qvec, numCandidates:2000, limit:50 } },
-  { $project: { c:{ $ifNull:["$classification","$Classification"] } } },
-  { $group: { _id:"$c", count:{ $sum:1 } } },
-  { $sort: { count:-1 } }
-])
