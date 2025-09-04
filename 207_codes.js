cbb6e70fe0d92149a54e02963f6aceb9ad71c6ec
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2025-09-04 15:56:04 -0500

    Delete 207_codes.js

--- 207_codes.js
+++
@@ -1,6 +0,0 @@
-db.taxonomy251.aggregate([
-  { $vectorSearch: { index:"nucc", path:"embedding", queryVector:qvec, numCandidates:3000, limit:50 } },
-  { $match: { $or:[ {code:{ $regex:/^207/ }}, {Code:{ $regex:/^207/ }} ] } },
-  { $limit: 10 },
-  { $project: { _id:0, code:{ $ifNull:["$code","$Code"] }, displayName:{ $ifNull:["$displayName","$Display Name"] }, score:{ $meta:"vectorSearchScore" } } }
-])
